c1f4363654568c4431bf908488ccc673ceacba5d
amicha02 <andreaszmichael6@gmail.com>
2021-04-27 23:52:56 -0400

    Update BDM_HW4_LastName.py

--- BDM_HW4_LastName.py
+++ BDM_HW4_LastName.py
@@ -7,43 +7,40 @@ import ast,datetime
 import pyspark.sql.functions as F
 import csv
 import sys
-def categorize(x):
-  if x == '452210' or x == '452311':
-    yield ('big_box_grocers')
-  elif x == '445120':
-    yield ('convenience_stores')
-  elif x == '722410':
-    yield ('drinking_places')
-  elif x == '722511':
-    yield  ('full_service_restaurants')
-  elif x == '722513':
-    yield ('limited_service_restaurants')
-  elif x == '446110' or x == '446191':
-    yield ('pharmacies_and_drug_stores')
-  elif x == '311811' or x == '722515':
-    yield ('snack_and_bakeries')
-  elif x in ['445210', '445220', '445230', '445291', '445292','445299']:
-    yield ('specialty_food_stores')
-  elif x == '445110':
-    yield ('supermarkets_except_convenience_stores')
-  else:
-    yield('whatever')
-def range_f(start,end):
-  list1 =[]
-  start = datetime.datetime.strptime(start.split('T')[0] , '%Y-%m-%d')
-  end = datetime.datetime.strptime(end.split('T')[0] , '%Y-%m-%d')
-  range = pd.date_range(start, end)
-  for date in range:
-    list1.append(date.strftime('%Y-%m-%d'))
-  yield list1[:-1]
+
 if __name__=='__main__':
+    def categorize(x):
+      if x == '452210' or x == '452311':
+        yield ('big_box_grocers')
+      elif x == '445120':
+        yield ('convenience_stores')
+      elif x == '722410':
+        yield ('drinking_places')
+      elif x == '722511':
+        yield  ('full_service_restaurants')
+      elif x == '722513':
+        yield ('limited_service_restaurants')
+      elif x == '446110' or x == '446191':
+        yield ('pharmacies_and_drug_stores')
+      elif x == '311811' or x == '722515':
+        yield ('snack_and_bakeries')
+      elif x in ['445210', '445220', '445230', '445291', '445292','445299']:
+        yield ('specialty_food_stores')
+      elif x == '445110':
+        yield ('supermarkets_except_convenience_stores')
+      else:
+        yield('whatever')
+    def range_f(start,end):
+      list1 =[]
+      start = datetime.datetime.strptime(start.split('T')[0] , '%Y-%m-%d')
+      end = datetime.datetime.strptime(end.split('T')[0] , '%Y-%m-%d')
+      range = pd.date_range(start, end)
+      for date in range:
+        list1.append(date.strftime('%Y-%m-%d'))
+      yield list1[:-1]
     sc = pyspark.SparkContext()
     sqlContext = sql.SQLContext(sc)
-    places = sc.textFile('hdfs:///data/share/bdm/core-places-nyc.csv') \
-    .map(lambda x: x.encode('utf-8').split(',')) \
-    .map(lambda x: (next(categorize(x[9])),x[1])) \
-    .filter(lambda x: x[0] != 'whatever') \
-    .collect()
+    places = sc.textFile('core-places-nyc.csv').smap(lambda x: x.split(',')).map(lambda x: (next(categorize(x[9])),x[1])).filter(lambda x: x[0] != 'whatever').collect()
     rdd_places =sc.parallelize(places)
     df_places = rdd_places.toDF(['category','store_id'])
     df = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/part-00000') \
