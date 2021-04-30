ec704db77b1cf9f3cacc4825668687f2a81529c7
amicha02 <andreaszmichael6@gmail.com>
2021-04-29 20:54:55 -0400

    Update BDM_HW4_Michael2.py

--- BDM_HW4_Michael2.py
+++ BDM_HW4_Michael2.py
@@ -32,7 +32,7 @@ if __name__=='__main__':
     def extractPlaces(partId, records):
         if partId==0:
             next(records)
-        reader = csv.reader([records.encode('utf-8')])
+        reader = csv.reader(records)
         for row in reader:
             (id,category) = (row[1],next(categorize(row[9])))
             if category != 'whatever':
@@ -46,16 +46,16 @@ if __name__=='__main__':
     def extractSchools(partId, list_of_records):
         if partId==0: 
             next(list_of_records) # skipping the first line
-        reader = csv.reader([list_of_records.encode('utf-8')])
+        reader = csv.reader(list_of_records)
         for row in reader:
                 (id, dates,visits) = (row[1], next(range_f(row[12], row[13])),row[16])
                 for i,date in enumerate(dates):
                   yield (id, (date,ast.literal_eval(visits)[i]))
     sc = pyspark.SparkContext()
     sqlContext = sql.SQLContext(sc)
-    places = sc.textFile('hdfs:///data/share/bdm/core-places-nyc.csv', use_unicode=True).cache()
+    places = sc.textFile('hdfs:///data/share/bdm/core-places-nyc.csv', use_unicode=False).cache()
     placesrdd= places.mapPartitionsWithIndex(extractPlaces)
-    patterns = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/part-00000', use_unicode=True).cache()
+    patterns = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/part-00000', use_unicode=False).cache()
     rdd = patterns.mapPartitionsWithIndex(extractSchools).join(placesrdd).values().map(lambda x: (x[0][0],x[0][1],x[1])).sortBy(lambda x: x[0])
     categories = ['big_box_grocers','convenience_stores','drinking_places','full_service_restaurants','limited_service_restaurants','pharmacies_and_drug_stores','snack_and_bakeries','specialty_food_stores','supermarkets_except_convenience_stores']
     df = rdd.toDF(['date','visits','category'])
