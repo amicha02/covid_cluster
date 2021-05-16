8b0584e4a4d3431be5965a9ae9dc4a43b080a76f
amicha02 <andreaszmichael6@gmail.com>
2021-05-16 14:28:27 -0400

    Update BDM_HW4_rdd.py

--- BDM_HW4_rdd.py
+++ BDM_HW4_rdd.py
@@ -9,6 +9,7 @@ import ast
 import statistics
 from datetime import timedelta
 import dateutil.parser
+from dateutil.relativedelta import relativedelta
 #filter the Core Places data set to extract the store IDs of interest
 def filterPOIs(CAT_CODES,CAT_GROUP,_, lines):
     if _==0:
@@ -28,7 +29,7 @@ def extractVisits(storeGroup, _, lines):
       if row[0] in storeGroup: 
           for index,value in enumerate(ast.literal_eval(row[16])): 
                 if (dateutil.parser.parse(row[12])+ datetime.timedelta(days=index)).year > 2018:
-                    yield ((storeGroup[row[0]],(dateutil.parser.parse(row[12])+datetime.timedelta(days=index)-dateutil.parser.parse('2020-01-01')).days),value)
+                    yield ((storeGroup[row[0]],(dateutil.parser.parse(row[12])+ relativedelta(day=1)-dateutil.parser.parse('2020-01-01')).days),value)
                 
 def remake_list(iterable,group_number):
     lst = list(iterable)
